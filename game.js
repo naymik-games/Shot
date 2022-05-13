adc129e05c412ffe8ea2f9f1023ade91815a0466
naymik-games <naymik@gmail.com>
2022-05-13 10:11:46 -0400

    figured out positioning

--- game.js
+++ game.js
@@ -37,21 +37,28 @@ class playGame extends Phaser.Scene {
 
 
     //this.cameras.main.setBackgroundColor(0x000000);
-    this.cameras.main.setBounds(-2100, -2100, 4200, 4200);
-    this.physics.world.setBounds(-2100, -2100, 4200, 4200);
+    this.cameras.main.setBounds(0, 0, 4200, 4200);
+    this.physics.world.setBounds(0, 0, 4200, 4200);
     this.cameras.main.setZoom(.5)
     /* this.add.image(-1024, -1024, 'bg').setOrigin(0);
     this.add.image(0, -1024, 'bg').setOrigin(0);
     this.add.image(-1024, 0, 'bg').setOrigin(0); */
-    var pic = this.add.image(0, 0, 'sky').setOrigin(.5).setScale(3);
+    var pic = this.add.image(0, 0, 'sky').setOrigin(0).setScale(3);
     var bounds = this.cameras.main.getBounds();
-    var gSize = 40
-    var g1 = this.add.grid(0, 0, bounds.width, bounds.height, gSize, gSize, 0x333333, 0, 0xcccccc, .5).setOrigin(.5);
-    this.target = this.add.image(13 * gSize, 0, 'target')
+    this.gSize = 40
+    var g1 = this.add.grid(0, 0, bounds.width, bounds.height, this.gSize, this.gSize, 0x333333, 0, 0xcccccc, .5).setOrigin(0);
+
+
+
+    this.target = this.add.image(13 * this.gSize, 0, 'target')
+    this.setTarget(52, 52)
+
+
+
     this.cursors = this.input.keyboard.createCursorKeys();
     this.spot = this.add.image(0, 0, 'particle').setTint(0xff0000).setAlpha(0)
 
-    this.player = this.physics.add.image(0, 0, 'scope').setScale(.5).setAlpha(0)
+    this.player = this.physics.add.image(2100, 2100, 'scope').setScale(.5).setAlpha(.1)
 
     this.player.setCollideWorldBounds(true);
 
@@ -68,7 +75,7 @@ class playGame extends Phaser.Scene {
 
     //pic.mask = new Phaser.Display.Masks.BitmapMask(this, spotlight);
 
-    this.text = this.add.text(32, 62).setScrollFactor(0).setFontSize(62).setColor('#000000');
+    this.text = this.add.text(32, 86).setScrollFactor(0).setFontSize(62).setColor('#000000');
     /* this.input.on("pointerdown", this.gemSelect, this);
      this.input.on("pointermove", this.drawPath, this);
      this.input.on("pointerup", this.removeGems, this);
@@ -109,6 +116,13 @@ class playGame extends Phaser.Scene {
       this.player.setVelocityY(300);
     }
   }
+  setTarget(col, row) {
+    var xpos = col * this.gSize
+    var ypos = row * this.gSize
+    this.target.setPosition(xpos, ypos)
+    var tbound = this.target.getBounds()
+    this.rect = new Phaser.Geom.Rectangle(tbound.x, tbound.y, tbound.width, tbound.height)
+  }
   toggleScope() {
     if (this.cameras.main.zoom == .5) {
       this.cameras.main.setZoom(3)
@@ -119,8 +133,8 @@ class playGame extends Phaser.Scene {
         duration: 300,
         alpha: 1
       })
-      this.cameras.main.setBounds(-2100 * 3, -1050 * 3, 4200 * 3, 4200 * 3);
-      this.physics.world.setBounds(-2100 * 3, -1050 * 3, 4200 * 3, 4200 * 3);
+      this.cameras.main.setBounds(0, 0, 4200 * 3, 4200 * 3);
+      this.physics.world.setBounds(0, 0, 4200 * 3, 4200 * 3);
     } else {
       this.cameras.main.setZoom(.5)
       //this.player.setAlpha(.2)
@@ -129,8 +143,8 @@ class playGame extends Phaser.Scene {
         duration: 300,
         alpha: .1
       })
-      this.cameras.main.setBounds(-2100, -2100, 4200, 4200);
-      this.physics.world.setBounds(-2100, -2100, 4200, 4200);
+      this.cameras.main.setBounds(0, 0, 4200, 4200);
+      this.physics.world.setBounds(0, 0, 4200, 4200);
     }
   }
   fire() {
@@ -143,13 +157,17 @@ class playGame extends Phaser.Scene {
     this.sound.play('shot')
     this.spot.setAlpha(1)
     this.spot.setPosition(this.player.x, this.player.y)
+    if (Phaser.Geom.Rectangle.ContainsPoint(this.rect, this.spot)) {
+      console.log('HIT')
+      this.setTarget(56, 52)
+    }
 
   }
   movePlayer(direction, force) {
     if (force > 50) {
       this.playerSpeed = 10
     } else {
-      this.playerSpeed = 5
+      this.playerSpeed = 1
     }
     if (direction === "up") {
       this.player.y -= this.playerSpeed;
